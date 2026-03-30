2d35209397a57af928f67b3559d3c17271d5d85b
yveseduardo97 <eduardoyves1997@gmail.com>
2026-03-29 22:26:18 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("Sistema funcionando");
+let produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+
+function mostrarProdutos() {
+    let lista = document.getElementById("lista-produtos");
+    lista.innerHTML = "";
+
+    produtos.forEach(produto => {
+        let div = document.createElement("div");
+        div.classList.add("card");
+
+        div.innerHTML = `
+            <h3>${produto.nome}</h3>
+            <p>${produto.descricao}</p>
+            <p><strong>Contato:</strong> ${produto.contato}</p>
+        `;
+
+        lista.appendChild(div);
+    });
+}
+
+mostrarProdutos();
